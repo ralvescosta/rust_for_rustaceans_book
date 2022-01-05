150dc7ba99ce1dd265418c79201a78d8fbe948f0
ralvescosta <rafael.rac.mg@gmail.com>
2022-01-05 10:56:35 -0300

    feat: multiple lifetimes

--- src/chapter_one/mod.rs
+++ src/chapter_one/mod.rs
@@ -116,3 +116,26 @@ pub fn listing_one_nine() {
     println!("last value of z: {}", z); // 'a
     println!();
 }
+
+/// Lifetimes - A type that needs to be generic over multiple lifetimes
+pub fn listing_one_tem() {
+    struct StrSplit<'s, 'p> {
+        delimiter: &'p str,
+        document: &'s str,
+    }
+    impl<'s, 'p> Iterator for StrSplit<'s, 'p> {
+        type Item = &'s str;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            todo!()
+        }
+    }
+
+    fn str_before(s: &str, c: char) -> Option<&str> {
+        StrSplit {
+            document: s,
+            delimiter: &c.to_string(),
+        }
+        .next()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,5 @@ fn main() {
     chapter_one::listing_one_seven();
     chapter_one::listing_one_eight();
     chapter_one::listing_one_nine();
+    chapter_one::listing_one_tem();
 }
